351ffa035a42e519f14be05c0e2883b585d33b49
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-08 17:18:58 +0200

    added css and style loaders; added css

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,8 +16,13 @@ module.exports = {
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
           plugins: ['@babel/plugin-proposal-class-properties']
-        },
+        }
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
     ],
   },
   plugins: [
