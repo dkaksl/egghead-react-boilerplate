b3f45abec64697de5f1f74baa1093cc4a3cff66d
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-12 10:20:51 +0200

    extracted functions

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ class App extends React.Component {
     state = {
         count: 0,
     };
+
+    increment = () => {
+        this.setState((state) => ({ count: state.count + 1 }))
+    }
+
+    decrement = () => {
+        this.setState((state) => ({ count: state.count - 1 }))
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello World!! live from webpack-dev-server</h1>
                 <h2>Count: {this.state.count}</h2>
-                <button onClick={() => this.setState((state) => ({ count: state.count + 1 }))}>+</button>
-                <button onClick={() => this.setState((state) => ({ count: state.count - 1 }))}>-</button>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
             </div>
         );
     }
