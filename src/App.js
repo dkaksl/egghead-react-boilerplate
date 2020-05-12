a1c6369fa74d2491f7b33a100b4eb6c067363e01
Amani Kilumanga <amani.kilumanga@gmail.com>
2020-05-12 10:25:14 +0200

    declared constant; added conditional styling to element

--- src/App.js
+++ src/App.js
@@ -14,10 +14,11 @@ class App extends React.Component {
     }
 
     render() {
+        const {count} = this.state
         return (
             <div>
                 <h1>Hello World!! live from webpack-dev-server</h1>
-                <h2>Count: {this.state.count}</h2>
+                <h2 className={count > 10 ? 'warning' : null}>Count: {this.state.count}</h2>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
             </div>
